e2000b7db355f94311f245eb3e8a27841a566551
ZugravuPaul <61908128+ZugravuPaul@users.noreply.github.com>
2023-09-05 09:11:04 +0300

    Add files via upload

---
+++ 3_Solution/demo_db/ATMLibrary/ATMLibrary/dbo/Tables/Carti.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Carti]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Titlu] NCHAR(40) NOT NULL, 
+    [Autor] NCHAR(40) NULL, 
+    [An_publicare] NCHAR(20) NULL
+)
---
+++ 3_Solution/demo_db/ATMLibrary/ATMLibrary/dbo/Tables/users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[users] (
+    [id]       INT            IDENTITY (1, 1) NOT NULL,
+    [nume] NCHAR(10) NOT NULL, 
+    [prenume] NCHAR(10) NOT NULL, 
+    [username] NVARCHAR (255) NOT NULL,
+    [password] NVARCHAR (255) NOT NULL,
+    [email]    NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    UNIQUE NONCLUSTERED ([email] ASC)
+);
+
