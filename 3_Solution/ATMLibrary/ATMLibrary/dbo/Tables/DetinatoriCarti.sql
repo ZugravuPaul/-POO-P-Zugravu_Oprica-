6e8850154f0fa9cc29f34eebd9126e947cd7d069
ZugravuPaul <61908128+ZugravuPaul@users.noreply.github.com>
2023-09-05 08:33:05 +0300

    Delete 3_Solution/ATMLibrary directory

--- 3_Solution/ATMLibrary/ATMLibrary/dbo/Tables/DetinatoriCarti.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[DetinatoriCarti]
-(
-	[Id] INT            IDENTITY (1, 1) NOT NULL, 
-    [IdUser] INT NOT NULL FOREIGN KEY references users(Id), 
-    [IdCarte] INT NOT NULL FOREIGN KEY references Carti(Id)
-
-)
